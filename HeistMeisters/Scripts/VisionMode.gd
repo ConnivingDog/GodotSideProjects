cd26f70846623723e70397663adaefb84986934b
Ronan Lina <connivinggod7@gmail.com>
2019-03-12 14:55:10 +0800

    re: vision mode toggling. (fix)

--- HeistMeisters/Scripts/VisionMode.gd
+++ HeistMeisters/Scripts/VisionMode.gd
@@ -1,4 +1,14 @@
 extends CanvasModulate
 
+const DARK = Color("100b2a")
+const NIGHTVISION = Color("369f41")
+
 func _ready():
-	pass
+	add_to_group("interface")
+	color = DARK
+
+func NightVision_mode():
+	color = NIGHTVISION
+
+func DarkVision_mode():
+	color = DARK
