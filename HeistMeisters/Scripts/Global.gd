d9f2b00f0639f2852361d68e0ede96c984cb0af8
Ronan Lina <connivinggod7@gmail.com>
2019-03-14 21:54:23 +0800

    numpad script. lockdoor and numpad still broken

--- HeistMeisters/Scripts/Global.gd
+++ HeistMeisters/Scripts/Global.gd
@@ -8,3 +8,6 @@ var destinations
 
 var nightvision_on_sfx = "res://SFX/nightvision.wav"
 var nightvision_off_sfx = "res://SFX/nightvision_off.wav" 
+
+var red_light = "res://GFX/Interface/PNG/dotRed.png"
+var green_light = "res://GFX/Interface/PNG/dotGreen.png"
---
+++ HeistMeisters/Scripts/NumberPad.gd
@@ -0,0 +1,48 @@
+extends Popup
+
+onready var display = $VSplitContainer/DisplayContainer/Display
+onready var light = $VSplitContainer/ButtonContainer/ButtonGrid/Light
+
+var combination = [4,1,5]
+var guess = []
+
+func _ready():
+	connect_buttons()
+
+func connect_buttons():
+	for child in $VSplitContainer/ButtonContainer/ButtonGrid.get_children():
+		if child is Button:
+			child.connect("pressed", self, "on_Button_pressed", [child.text])
+
+func _on_Button_pressed(button):
+	if button == "Ok":
+		check_guess()
+	else:
+		enter(int(button))
+
+func check_guess():
+	if guess == combination:
+		print("correct")
+		light.texture = Global.green_light
+		$Timer.start()
+	else:
+		reset_lock()
+
+func enter(button):
+	guess.append(button)
+	update_display()
+
+func reset_lock():
+	light.texture = Global.red_light
+	display.clear()
+	guess.clear() 
+
+func update_display():
+	display.bbcode_text = "[center]" + PoolStringArray(guess).join("") + "[/center]"
+	if guess.size() == combination.size():
+		check_guess()
+
+
+func _on_Timer_timeout():
+	hide()
+	reset_lock()
