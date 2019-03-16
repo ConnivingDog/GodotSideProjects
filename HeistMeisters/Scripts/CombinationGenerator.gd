3f489e6c7e5f8a4979a3971a0f82c08b95a9babf
Ronan Lina <connivinggod7@gmail.com>
2019-03-16 09:08:58 +0800

    added combination generator

---
+++ HeistMeisters/Scripts/CombinationGenerator.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func _ready():
+	generator_combination(length)
+
+func generator_combination(length):
+	var combination = []
+	for number in range(length):
+		randomize()
+		combination.append(randi() % 10)
+		
+		return combination
+		
