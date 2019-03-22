a0cc024bb9a6f98078c81a3026e297199d55e921
Ronan Lina <connivinggod7@gmail.com>
2019-03-23 02:52:58 +0800

    tutorial level SFXs and tween anims

--- HeistMeisters/Scripts/Tutorial.gd
+++ HeistMeisters/Scripts/Tutorial.gd
@@ -6,8 +6,13 @@ func _ready():
 func update_pointer_position():
 	var pointer = $ObjectivePointer
 	var marker = $ObjectiveMarkers.get_child(0)
-	pointer.position = marker.position
+	
+	$Tween.interpolate_property(pointer, "position", pointer.position, marker.position,
+			0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	$Tween.start()
+	
 	$ObjectiveMarkers.remove_child(marker)
+	$AudioStreamPlayer.play()
 
 func _on_ObjectiveMove_body_entered(body):
 	update_pointer_position()
