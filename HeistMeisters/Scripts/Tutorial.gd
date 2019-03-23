7442da5653a05afb98174b8695eac54ed650ee6e
Ronan Lina <connivinggod7@gmail.com>
2019-03-24 00:14:51 +0800

    objective marker bug fixed

--- HeistMeisters/Scripts/Tutorial.gd
+++ HeistMeisters/Scripts/Tutorial.gd
@@ -1,29 +1,26 @@
 extends Node2D
 
 func _ready():
-	update_pointer_position()
+	update_pointer_position(0)
 
-func update_pointer_position():
+func update_pointer_position(number):
 	var pointer = $ObjectivePointer
-	var marker = $ObjectiveMarkers.get_child(0)
-	
+	var marker = $ObjectiveMarkers.get_child(number)
 	$Tween.interpolate_property(pointer, "position", pointer.position, marker.position,
 			0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
 	$Tween.start()
-	
-	$ObjectiveMarkers.remove_child(marker)
 	$AudioStreamPlayer.play()
 
 func _on_ObjectiveMove_body_entered(body):
-	update_pointer_position()
+	update_pointer_position(1)
 
 func _on_ObjectiveDoor_body_entered(body):
-	update_pointer_position()
+	update_pointer_position(2)
 
 
 func _on_ObjectiveNightVision_body_entered(body):
-	update_pointer_position()
+	update_pointer_position(3)
 
 
 func _on_BriefCase_body_entered(body):
-	update_pointer_position()
+	update_pointer_position(4)
